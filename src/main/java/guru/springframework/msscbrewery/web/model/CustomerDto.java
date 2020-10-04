1e2a32383cfb8db80b01f0a03e537361d328d29c
JuanCalMun <juancalmun@gmail.com>
2020-10-04 18:20:41 +0200

    #3 Add validation in CustomerDto, min and max better in one line

--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -5,9 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -18,7 +17,6 @@ public class CustomerDto {
     private UUID id;
 
     @NotBlank
-    @Min(3)
-    @Max(100)
+    @Size(min = 3, max = 100)
     private String name;
 }
